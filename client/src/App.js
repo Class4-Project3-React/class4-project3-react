937128e81ecd93b0c34feb823a33ba99f1a77d60
Jaewoneeee <chou60400@naver.com>
2021-12-30 17:56:18 +0900

    jaewon test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./assets/css/App.css";
 
 function App() {
-  return <div className="App">21.12.30 0425 이상 무 - 권원현</div>;
+  return (
+    <div className="App">
+      <p>21.12.30 0425 이상 무 - 권원현</p>
+      <p>재원 제대로 작동확인</p>
+    </div>
+  );
 }
 
 export default App;
