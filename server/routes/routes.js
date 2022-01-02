6362ecc6a349714b12910f60d6d86be1bd3cfe8b
Jaewoneeee <chou60400@naver.com>
2022-01-02 23:18:07 +0900

    0102 23:18

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,4 +4,6 @@ const controllers = require("../controllers/controllers");
 
 router.get('/', controllers.TestControllers); // 권원현 server 및 db 테스트용 router ('/' url 필요하신 분은 삭제하고 쓰셔도 됨)
 
+
+
 module.exports = router;
