a6dba7741f6745e93ee055929f8b9b07ab53d256
Jaewoneeee <chou60400@naver.com>
2022-01-02 22:15:28 +0900

    0102

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,7 @@ import Main from "./components/pages/main";
 
 function App() {
   return (
-    <div className="App">
-      <p>21.12.30 0425 이상 무 - 권원현</p>
-      <p>재원 제대로 작동확인</p>
-    </div>
+    <Main />
   );
 }
 
