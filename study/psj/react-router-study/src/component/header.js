9d200424232f0f6edd994e972468907c996ab3ba
seungjackie <sunsun714@naver.com>
2022-01-04 20:08:46 +0900

    승재 feautre commit

---
+++ study/psj/react-router-study/src/component/header.js
@@ -0,0 +1,3 @@
+import React from 'react'; function Header() { return ( <header className='header'> <strong>Header</strong> <ul> <li> <button>홈</button> </li> <li> <a href=''>프로필</a> </li> </ul> </header> ); } export default Header;
+
+출처: https://tigger.dev/entry/React-Router를-활용해-원하는-페이지로-이동하기 [개발 여행]
