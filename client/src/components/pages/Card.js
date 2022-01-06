cb01e4bdea0306d160ee6f7d923fe741c17a6828
seungjackie <sunsun714@naver.com>
2022-01-06 21:12:04 +0900

    승재 데브랑~

--- client/src/components/pages/Card.js
+++ client/src/components/pages/Card.js
@@ -1,13 +1,15 @@
 import React from "react";
 // import "./Card.css"
-import realman from '../../assets/img/realman.png'
+// import realman from '../../assets/img/realman.png'
 import styled from "styled-components";
-import scr from '../../assets/img/pushup.png'
+import HomeTMain from '../../assets/img/HomeTMain.png'
+import CrossfitMain from '../../assets/img/CrossfitMain.png';
+import SportMain from '../../assets/img/SportMain.png'
 
 
 const MainNav = styled.div`
   .card-container{
-    padding: 10;
+    /* padding: 10; */
     margin-top: 100px;
     margin-left: 50px;
     width: 300px;
@@ -15,10 +17,13 @@ const MainNav = styled.div`
     box-sizing: 0px 0px 15px -5px;
     transition: 0.3s;
     /* 애니메이션 추가 */
+    /* 애니메이션 졸라 안되네 */
     animation: ease-in;
     background-color: lightblue;
+    position: static;
+    display: inline-block;
 }
-
+/* 이것도 지랄이네 */
 .card-container:hover{
     transform: scale (1.1);
     box-sizing: 0px 0px 15px 0px;
@@ -30,11 +35,7 @@ const MainNav = styled.div`
     height: 200px;
 }
 
-.Image img{
- background-color: red;
- width: 100%;
- height: 600px;
-}
+
 
 
 .card-contetnt{
@@ -47,15 +48,16 @@ h3,p{
      padding: 0;
 }
 
-.card-title{
+/* .card-title{
     margin-bottom: 0.5rem;
-}
+} */
 
 .btn {
     display: flex;
     justify-content: center;
 }
 
+/* 버튼 css */
 .btn button{
     padding: 0.5rem;
     background-color: white;
@@ -65,6 +67,7 @@ h3,p{
     margin-bottom: 0.5rem;
 }
 
+/* 버튼 애니메이션 */
 .btn:hover{
     background: gray;
     transform:scale(1.2)
@@ -79,7 +82,7 @@ a{
 }
 
 `
-const imgData ={
+const ImgDataHome ={
     "images" : [
         "smallyoga",
         "smallhealth",
@@ -88,11 +91,35 @@ const imgData ={
     ]
 }
 
-//경로  public img에 되어있음
-const MyImages = imgData.images.map( img =>{
+const ImgDataSport ={
+    "images" : [
+        "sport1",
+        "sport2",
+        "sport3"
+        
+    ]
+}
+
+const ImgDataCross ={
+    "images" : [
+        "crossfit1",
+        "crossfit2",
+        "crossfit3"
+        
+    ]
+}
+
+const MyImagesH = ImgDataHome.images.map( img =>{
     return <img src={require('../../assets/img/' + img +'.png')} />
 })
 
+const MyImagesS = ImgDataSport.images.map( img =>{
+    return <img src={require('../../assets/img/' + img +'.png')} />
+})
+
+const MyImagesC = ImgDataCross.images.map( img =>{
+    return <img src={require('../../assets/img/' + img +'.png')} />
+})
     
 
 
@@ -101,14 +128,32 @@ function Card({title,body}) {
 
     const Results = () => (
         <div id="results" className="search-results">
-          {MyImages}
+          {MyImagesH}
           
         </div>
       )
+    const Results1 = () => (
+    <div id="results1" className="search-results1">
+        {MyImagesS}
+        
+    </div>
+    )
+    const Results2 = () => (
+    <div id="results2" className="search-results2">
+        {MyImagesC}
+        
+    </div>
+    )
 
 
       const [showResults, setShowResults] = React.useState(false)
       const onClick = () => setShowResults(true)
+
+      const [showResults1, setShowResults1] = React.useState(false)
+      const onClick1 = () => setShowResults1(true)
+
+      const [showResults2, setShowResults2] = React.useState(false)
+      const onClick2 = () => setShowResults2(true)
         
       
     
@@ -116,23 +161,21 @@ function Card({title,body}) {
         <>
         
             <MainNav>
-                <div className="Image">
-                    <img src={realman} />
-                </div>
+                
                 
 
                 <div className="card-container" border="primary" style={{ width: '18rem' }}>
                     <div className="image-container">
                         <div className="img">
-                            <img src={scr} />
+                            <img src={HomeTMain} />
                         </div>
                     </div>
                     <div className="card-content" border="secondary" style={{ width: '18rem' }}>
                         <div className="card-title">
-                            <h2>{title}</h2>
+                            <h2>HomeTranining</h2>
                         </div>
                         <div className="card-body">
-                            <p>{body}</p>
+                            <p>홈트 내<br/>용<br/>용<br/>용</p>
                         </div>
                     </div>
                     
@@ -149,24 +192,23 @@ function Card({title,body}) {
                 <div className="card-container">
                     <div className="image-container">
                         <div className="img">
-                            <img src={scr} />
+                            <img src={SportMain} />
                         </div>
                     </div>
                     <div className="card-content">
                         <div className="card-title">
-                            <h2>{title}</h2>
+                            <h2>Sports</h2>
                         </div>
                         <div className="card-body">
-                            <p>{body}</p>
+                            <p>스포츠 내용<br/>용<br/>용<br/>용</p>
                         </div>
                     </div>
 
                     <div className="btn">
-                        <button >
-                            <a>
-                                여기를 클릭하시면 밑에 이미지 띄울 예정
-                            </a>
-                        </button>
+                        <div>
+                        <input type="submit" value="Search" onClick={onClick1} />
+                        { showResults1 ? <Results1 /> : null }
+                        </div>
                     </div>
                 </div>
 
@@ -174,24 +216,23 @@ function Card({title,body}) {
                 <div className="card-container" style={{ width: '18rem' }}>
                     <div className="image-container">
                         <div className="img">
-                            <img src={scr} />
+                            <img src={CrossfitMain} />
                         </div>
                     </div>
                     <div className="card-content">
                         <div className="card-title">
-                            <h2>{title}</h2>
+                            <h2>Crossfit</h2>
                         </div>
                         <div className="card-body">
-                            <p>{body}</p>
+                            <p>크로스핏 내용<br/>용<br/>용<br/>용</p>
                         </div>
                     </div>
 
                     <div className="btn">
-                        <button>
-                            <a>
-                                여기를 클릭하시면 밑에 이미지 띄울 예정
-                            </a>
-                        </button>
+                        <div>
+                        <input type="submit" value="Search" onClick={onClick2} />
+                        { showResults2 ? <Results2 /> : null }
+                        </div>
                     </div>
                 </div>
             </MainNav>
