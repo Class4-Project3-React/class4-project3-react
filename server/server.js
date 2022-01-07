bda0cdaf7c79597108de898b0ef9e7bd2ca06e45
jhin93 <scene1993@gmail.com>
2022-01-07 17:35:03 +0900

    1/7 로그인,회원가입 로직 적용 - 작동x

--- server/server.js
+++ server/server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const router = require("./routes/routes");
 const cors = require("cors");
+const session = require('express-session');
 
 app.use(cors());
 app.use("/", router);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    name: 'session',
+    secret: 'my_secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 3600 * 1000, // 1hr
+    }
+}));
 
 const port = 3001;
 
