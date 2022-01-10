2073d21bf4caa99163679aa593e71dcc64515a33
WONHYUN KWON <mywonhyuni@naver.com>
2022-01-11 02:07:08 +0900

    220111_0206_whkwon_clear cache and add my work

--- client/src/containers/Contents_Article.js
+++ client/src/containers/Contents_Article.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux"
 import ContentsArticle from "../components/commons/Contents_Article";
+import { detail_article } from "../modules/Contents_Article";
 
 function ArticleContainer() {
-    const todos = useSelector(state => state)
+    const todos = useSelector(state => state.contArticle);
+    const dispatch = useDispatch();
+
+    const onView = () => dispatch(detail_article());
+
+    return <ContentsArticle todos={todos} onView={onView} />
 };
+
+export default ArticleContainer
--- client/src/modules/Contents_Article.js
+++ client/src/modules/Contents_Article.js
@@ -1,27 +1,80 @@
+import axios from "axios";
+
 // 1) action type
-const DETAIL_ARTICLE = 'contents/article/DETAIL_ARTICLE';
+const DETAIL_ARTICLE = "contents/article/DETAIL_ARTICLE";
 
 // 2) action creator
-export const detail_article = () => ({
-    type : DETAIL_ARTICLE,
+export const detail_article = (no) => ({
+  type: DETAIL_ARTICLE,
+  no,
 });
+
+axios
+  .get("http://localhost:3001/contents")
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error.response);
+  });
 
 const initialState = [
   {
-    media: 1,
-    img: "",
+    no: 1,
+    media: "asfjad",
+    editor: "권원현",
+    date: 123,
+    title: "asdf",
+    desc: "adf",
+    img: "leejung1",
+  },
+  {
+    no: 2,
+    media: "asfjad",
+    editor: "권원현",
+    date: 123,
+    title: "asdf",
+    desc: "adf",
+    img: "leejung2",
+  },
+  {
+    no: 3,
+    media: "asfjad",
+    editor: "권원현",
+    date: 123,
+    title: "asdf",
+    desc: "adf",
+    img: "leejung3",
+  },
+  {
+    no: 4,
+    media: "asfjad",
+    editor: "권원현",
+    date: 123,
+    title: "asdf",
+    desc: "adf",
+    img: "leejung4",
   },
 ];
 
 // 3) reducer
 export default function contArticle(state = initialState, action) {
-    switch (action.type) {
-        case DETAIL_ARTICLE:
-            return state.map(
-                todo =>
-                    todo.media = "testid"
-            )
-        default:
-            return state;
-    };
-};
+  switch (action.type) {
+    case DETAIL_ARTICLE:
+      return state;
+    default:
+      return state;
+  }
+}
+
+// axios
+//   .post("http://localhost:3001/contents", {
+//     no: action.no,
+//     userPassword: "4567",
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error.response);
+//   });
