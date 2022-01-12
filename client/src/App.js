06fa6f38e9e9b9a22d6b8a1243ec1deb55bf08c4
jhin93 <scene1993@gmail.com>
2022-01-12 15:12:31 +0900

    주석제거

--- client/src/App.js
+++ client/src/App.js
@@ -4,35 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, Contents, BordList, User, Login, Logout, Register, MyPage, LikePage } from './components/pages';
 import Menu from './components/Menu';
 // import Card from "./components/pages/Card";
-// import { useEffect, useState } from 'react';
 
 function App() {
-  // // 로그인 상태 관리
-  // const [isLogin, setIsLogin] = useState(false)
-
-  // useEffect(() => {
-  //   if(sessionStorage.getItem('user_id') === null){
-  //     // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 없다면
-  //       console.log('isLogin ?? :: ', isLogin)
-  //   } else {
-  //   // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 있다면
-  //   // 로그인 상태 변경
-  //     setIsLogin(true)
-  //     console.log('isLogin ?? :: ', isLogin)
-  //   }
-  // })
-
   return (
     <>
       {/* <Main/> */}
       {/* <Menu /> */}
             <Routes>
-              {/* {
-                // 로그인된 상태면 메인페이지로 이동하는 로그인 체크 로직(삼항 연산자)
-                isLogin ?
-                <Route exact path="/" element={<Home/>}/> :
-                <Route path="/login" element={<Login />}/>
-              } */}
               <Route exact path="/" element={<Home/>}/>
               <Route path="/contents" element={<Contents/>}/>
               <Route path="/bordlist" element={<BordList/>}/>
