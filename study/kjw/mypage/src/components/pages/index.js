70b1b73f063dfc900070fa8c36d13f1f0c6201bb
Jaewoneeee <93761302+Jaewoneeee@users.noreply.github.com>
2022-01-13 17:09:20 +0900

    jaewon merge commit

--- study/kjw/mypage/src/components/pages/index.js
+++
@@ -1,9 +0,0 @@
-// export { default as Home } from './Home';
-export { default as Contents } from './Contents';
-// export { default as User} from './User'
-// export { default as BoardList} from './BoardList'
-export { default as LikePage} from './LikePage'
-export { default as MyPage} from './MyPage'
-// export { default as Test} from './Test'
-// export { default as Login} from './Login'
-// export { default as Register} from './Register'
--- study/kjw/server/models/models copy.js
+++
@@ -1,3 +0,0 @@
-const Mypage = require("./Mypage_models");
-
-module.exports = { Mypage };
