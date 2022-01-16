c601cf1a65650e670ee41c7a24f35d9657f66e6c
jhin93 <scene1993@gmail.com>
2022-01-16 15:40:00 +0900

    로그인 get, 로그인 post, 회원가입 get 구현

--- server/models/Login_models.js
+++ server/models/Login_models.js
@@ -3,6 +3,7 @@
 
 const con = require("../utils/db");
 const modelExports = (module.exports = {});
+const controllers = require("../controllers/controllers");
 
 modelExports.ReactTest = () => {
     return new Promise((resolve, reject) => {
@@ -31,6 +32,65 @@ modelExports.ReactTest = () => {
         });
     });
 };
+
+// 로그인 GET
+modelExports.getLoginDB = () => {
+    console.log("login get 시도")
+}
+
+// 로그인 POST
+modelExports.postLoginDB = () => {
+    return new Promise ((resolve, reject) => {
+        // controllers로부터 req.query.user_id, req.query.user_pw를 받아옴.
+        const user_id = controllers.userId
+        const user_pw = controllers.userPw
+        // 입력된 id 와 동일한 id 가 mysql 에 있는 지 확인
+        const sql1 = 'SELECT COUNT(*) AS result FROM user_inform WHERE user_id = ?'
+        con.query(sql1, user_id, (err, data) => {
+            if(!err) {
+                // 결과값이 1보다 작다면(동일한 id 가 없다면)
+                if(data[0].result < 1) {
+                    resolve({ 'msg': '입력하신 id 가 일치하지 않습니다.'})
+                } else { // 동일한 id 가 있으면 비밀번호 일치 확인
+                    const sql2 = `SELECT 
+                                    CASE (SELECT COUNT(*) FROM user_inform WHERE user_id = ? AND user_pw = ?)
+                                        WHEN '0' THEN NULL
+                                        ELSE (SELECT user_id FROM user_inform WHERE user_id = ? AND user_pw = ?)
+                                    END AS userId
+                                    , CASE (SELECT COUNT(*) FROM user_inform WHERE user_id = ? AND user_pw = ?)
+                                        WHEN '0' THEN NULL
+                                        ELSE (SELECT user_pw FROM user_inform WHERE user_id = ? AND user_pw = ?)
+                                    END AS userPw`;
+                    // sql 란에 필요한 parameter 값을 순서대로 기재
+                    const params = [user_id, user_pw, user_id, user_pw, user_id, user_pw, user_id, user_pw]
+                    con.query(sql2, params, (err, data) => {
+                        if(!err) {
+                            resolve(data[0])
+                        } else {
+                            resolve(err)
+                        }
+                    })
+                }
+            } else {
+                resolve('에러 : ', err)
+            }
+        })
+
+    });
+}
+
+// 회원가입 GET
+modelExports.getRegisterDB = () => {
+    console.log("register get 시도")
+}
+
+// 회언가입 POST
+
+
+
+
+
+
 
 
 
